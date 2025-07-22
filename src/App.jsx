dc2faef78b4912998a72c9d7f3c651744cb8b7a0
Micheni58 <michenicaleb@gmail.com>
2025-07-22 23:49:32 +0300

    Add login and signup forms with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Footer from './Components/Footer/Footer'
 import './App.css'
-
+import SignUp from './Components/Authentication/SignUp'
+import Login from './Components/Authentication/Login'
 function App() {
 
   return (
     <>
       <h1>Recipe App</h1>
-      <Footer />
+      {/* <SignUp />
+      <Login /> */}
+      {/* <Footer /> */}
     </>
   )
 }
