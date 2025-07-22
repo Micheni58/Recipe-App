ca03c53f04a8ab9be3da6ceaffa3c399322604f5
ryangachahic <ryangachahi230@gmail.com>
2025-07-22 15:16:04 +0300

    installation of tailwind

--- src/Components/Form/form.jsx
+++ src/Components/Form/form.jsx
@@ -1,11 +1,57 @@
-function AddForm () {
-    return(
-        <form className="form">
-            <input type="text" placeholder="Enter your name" />
-            <input type="email" placeholder="Enter your email" />
-            <input type="password" placeholder="Enter your password" />
-            <button type="submit">Submit</button>
-        </form> 
-    )
+import { useState } from "react";
+import "./Form.css";
+
+function AddRecipeForm({ onAddRecipe }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    ingredients: "",
+    instructions: "",
+    cookingTime: "",
+    image: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Convert comma-separated ingredients to array
+    const newRecipe = {
+      ...formData,
+      ingredients: formData.ingredients.split(",").map(i => i.trim()),
+    };
+
+    // Send to parent (App or RecipeList)
+    onAddRecipe(newRecipe);
+
+    // Reset form
+    setFormData({
+      title: "",
+      description: "",
+      ingredients: "",
+      instructions: "",
+      cookingTime: "",
+     
+    });
+  }
+
+  return (
+    <form id="formdata" onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+      <input name="title" placeholder="Recipe Name" value={formData.title} onChange={handleChange} required /><br />
+      <textarea name="description" placeholder="Description" value={formData.description} onChange={handleChange} required /><br/>
+      <textarea name="ingredients" placeholder="Ingredients (comma separated)" value={formData.ingredients} onChange={handleChange} required /><br/>
+      <textarea name="instructions" placeholder="Instructions" value={formData.instructions} onChange={handleChange} required /><br/>
+      <input name="cookingTime" type="number" placeholder="Cooking Time (min)" value={formData.cookingTime} onChange={handleChange} required /><br/>
+      <input name="image" type="url" placeholder="Image URL" value={formData.image} onChange={handleChange} /><br/>
+      <button id="button"type="submit">Add Recipe</button>
+    </form>
+  );
 }
-export default AddForm;
+
+export default AddRecipeForm;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  tailwindcss: {
+    config: './tailwind.config.js',
+  },
   plugins: [react()],
 })
