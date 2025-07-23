38afcec4a9b57dcac2ecb1520469b227b4038710
Micheni58 <michenicaleb@gmail.com>
2025-07-23 10:47:08 +0300

    Add home page with routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
 import './App.css'
+import HomePage from './pages/Home/Home'
+// import Login from './pages/Authentication/Login'    
+// import Signup from './pages/Authentication/Signup'   
 
 function App() {
-
   return (
-    <>
-      <h1>Recipe App</h1>
-      <Footer />
-    </>
+    <Router>
+      <div className="App">
+        <h1>Recipe App</h1>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
-export default App
+export default App;
