74c9116f982fcd5b367fa1269d9662de06bfa68b
Micheni58 <michenicaleb@gmail.com>
2025-07-23 12:56:31 +0300

    Display login and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
 import './App.css'
 import SignUp from './Components/Authentication/SignUp'
-// import Login from './Components/Authentication/Login'
-// import HomePage from './pages/Home/Home'
+import Login from './Components/Authentication/Login'
+import HomePage from './pages/Home/Home'
 
 function App() {
   return (
