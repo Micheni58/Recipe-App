0ccd9f441c77fee849b6486baf0f25c772c35306
irene29-hub <muemarenee@gmail.com>
2025-07-24 00:46:47 +0300

    add changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react';
+import React from "react";
 import "./Navbar.css"
-const Navbar = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
-    const handleAuthClick = () => {
-        setIsLoggedIn(!isLoggedIn);
-    };
 
+const Navbar = () => {
+    
     return(
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/recipes">About</a></li>
-                <li><a href="/categories">Contacts</a></li>
-                <li><a href="/favorites">Favorites</a></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contacts">Contacts</a></li>
             </ul>
         </nav>
     );
