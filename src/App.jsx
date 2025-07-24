2d7ad214db1fcbdf1b251eb7e75703d38c7c7c4c
Micheni58 <michenicaleb@gmail.com>
2025-07-24 17:16:23 +0300

    Modify home page with styling and add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUp from './Components/Authentication/SignUp';
-import Login from './Components/Authentication/Login';
-import HomePage from './pages/Home/Home';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
-import Dashboard from './pages/Dashboard/Dashboard';
-import './App.css';
-// import Buttonicon from './Components/Button/Button';
-
-
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+import { RecipeProvider } from "./context/RecipeContext"
+import { ToastProvider } from "./context/ToastContext"
+import Home from "./Pages/Home/Home"
+import Login from "./Pages/Login/Login"
+import Signup from "./Pages/Signup/Signup"
+import Dashboard from "./Pages/Dashboard/Dashboard"
+import BrowseRecipes from "./Pages/BrowseRecipes/BrowseRecipes"
+import Favorites from "./Pages/Favorites/Favorites"
+import AddRecipe from "./Pages/AddRecipe/AddRecipe"
+import RecipeDetail from "./Components/RecipeDetail/RecipeDetail"
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
+import Button from "./Components/Button/Button"
+import "./App.css"
 
 function App() {
   return (
-
     <Router>
-      <div className="App">
-        <Navbar />
-        <h1>Recipe App</h1>
-
-        {/* <Buttonicon /> */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-
-        <Footer />
-      </div>
+      <AuthProvider>
+        <RecipeProvider>
+          <ToastProvider>
+            <div className="App">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/browse-recipes" element={<BrowseRecipes />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/add-recipe" element={<AddRecipe />} />
+                <Route path="/recipe/:id" element={<RecipeDetail />} />
+              </Routes>
+              <Button />
+              <Footer />
+            </div>
+          </ToastProvider>
+        </RecipeProvider>
+      </AuthProvider>
     </Router>
-  );
-
+  )
 }
 
-export default App;
+export default App
