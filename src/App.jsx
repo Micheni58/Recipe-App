5dacb9de75e179a3a2d6bb83c171161098b6fa5e
monica <monica.njoki@student.moringaschool.com>
2025-07-24 17:31:07 +0300

    Monica

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import './App.css';
-import Buttonicon from './Components/Button/Button';
 function App() {
 
   return (
     <>
       <h1>Recipe App</h1>
-      <Buttonicon />
     </>
   )
 }
